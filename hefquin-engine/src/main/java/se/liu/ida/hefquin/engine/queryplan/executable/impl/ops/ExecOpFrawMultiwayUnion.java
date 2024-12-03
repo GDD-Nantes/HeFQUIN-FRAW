4b9d09a639f1d6baae6138a0061410d244086191
theray1 <theray1@outlook.fr>
2024-12-03 15:57:59 +0100

    Union operator picks a new branch every time all it's children have been processed

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpFrawMultiwayUnion.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpFrawMultiwayUnion.java
@@ -9,6 +9,7 @@ import org.apache.jena.sparql.engine.binding.BindingBuilder;
 import se.liu.ida.hefquin.base.data.SolutionMapping;
 import se.liu.ida.hefquin.base.data.impl.SolutionMappingImpl;
 import se.liu.ida.hefquin.base.data.utils.SolutionMappingUtils;
+import se.liu.ida.hefquin.engine.queryplan.executable.ExecOpExecutionException;
 import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultBlock;
 import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultElementSink;
 import se.liu.ida.hefquin.engine.queryplan.executable.impl.CollectingIntermediateResultElementSink;
@@ -23,10 +24,13 @@ import static se.liu.ida.hefquin.jenaintegration.sparql.HeFQUINConstants.MAPPING
 public class ExecOpFrawMultiwayUnion extends ExecOpMultiwayUnion{
 
     private int chosenChild;
+    private Random random;
+    private int numberOfChildrenProcessed;
 
     public ExecOpFrawMultiwayUnion(int numberOfChildren, boolean collectExceptions) {
         super(numberOfChildren, collectExceptions);
-        this.chosenChild = (new Random()).nextInt(numberOfChildren);
+        this.random = new Random();
+        this.chosenChild = random.nextInt(numberOfChildren);
     }
 
     @Override
@@ -34,31 +38,47 @@ public class ExecOpFrawMultiwayUnion extends ExecOpMultiwayUnion{
                                               final IntermediateResultBlock input,
                                               final IntermediateResultElementSink sink,
                                               final ExecutionContext execCxt) {
-        CollectingIntermediateResultElementSink tempSink = new CollectingIntermediateResultElementSink();
 
-        if(chosenChild == x)
+        if(chosenChild == x){
+            CollectingIntermediateResultElementSink tempSink = new CollectingIntermediateResultElementSink();
+
             super._processBlockFromXthChild(x, input, tempSink, execCxt);
 
-        tempSink.getCollectedSolutionMappings().forEach(
-                solutionMapping -> {
-                    Binding binding = solutionMapping.asJenaBinding();
+            tempSink.getCollectedSolutionMappings().forEach(
+                    solutionMapping -> {
+                        Binding binding = solutionMapping.asJenaBinding();
 
-                    BindingBuilder bb = BindingBuilder.create();
+                        BindingBuilder bb = BindingBuilder.create();
 
-                    for (Iterator<Var> it = binding.vars(); it.hasNext(); ) {
-                        Var var = it.next();
-                        if(!var.equals(MAPPING_PROBABILITY)){
-                            Node node = binding.get(var);
-                            bb.add(var, node);
-                        }else if(!bb.contains(MAPPING_PROBABILITY)) {
-                            Double newProbability = ((Double) binding.get(MAPPING_PROBABILITY).getLiteralValue()) / numberOfChildren;
-                            bb.add(MAPPING_PROBABILITY, NodeFactory.createLiteral(String.valueOf(newProbability), XSDDatatype.XSDdouble));
+                        for (Iterator<Var> it = binding.vars(); it.hasNext(); ) {
+                            Var var = it.next();
+                            if(!var.equals(MAPPING_PROBABILITY)){
+                                Node node = binding.get(var);
+                                bb.add(var, node);
+                            }else if(!bb.contains(MAPPING_PROBABILITY)) {
+                                Double newProbability = ((Double) binding.get(MAPPING_PROBABILITY).getLiteralValue()) / numberOfChildren;
+                                bb.add(MAPPING_PROBABILITY, NodeFactory.createLiteral(String.valueOf(newProbability), XSDDatatype.XSDdouble));
+                            }
                         }
-                    }
 
-                    sink.send(new SolutionMappingImpl(bb.build()));
-                }
-        );
+                        sink.send(new SolutionMappingImpl(bb.build()));
+                    }
+            );
+        }
+    }
+
+    @Override
+    protected void _wrapUpForXthChild(int x,
+                                  IntermediateResultElementSink sink,
+                                  ExecutionContext execCxt) {
+        this.numberOfChildrenProcessed++;
+
+        if(allChildrenProcessed())
+            this.chosenChild = random.nextInt(numberOfChildren);
+    }
+
+    private boolean allChildrenProcessed(){
+        return this.numberOfChildrenProcessed == this.numberOfChildren;
     }
 }
 
