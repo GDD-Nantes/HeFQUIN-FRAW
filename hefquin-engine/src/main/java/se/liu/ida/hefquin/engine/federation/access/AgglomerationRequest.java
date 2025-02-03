f5a7d59f97ef4a4a244c261fdd9fa74e0eade037
theray1 <theray1@outlook.fr>
2025-02-03 14:47:54 +0100

    Removed unused files

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/AgglomerationRequest.java
+++
@@ -1,6 +0,0 @@
-package se.liu.ida.hefquin.engine.federation.access;
-
-// Maybe there is more to it, but right now type specification should be enough, so empty interface for now
-public interface AgglomerationRequest extends DataRetrievalRequest {
-
-}
