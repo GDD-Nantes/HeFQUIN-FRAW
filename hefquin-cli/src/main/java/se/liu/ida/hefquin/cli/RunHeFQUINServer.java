e9254f8662ce0084464d7cff8cb35970d5c09107
theray1 <theray1@outlook.fr>
2025-02-14 10:41:20 +0100

    increased max form content size, in order to allow for larger queries like instantied union over join plan queries with a big source selection

--- hefquin-cli/src/main/java/se/liu/ida/hefquin/cli/RunHeFQUINServer.java
+++ hefquin-cli/src/main/java/se/liu/ida/hefquin/cli/RunHeFQUINServer.java
@@ -1,12 +1,11 @@
 package se.liu.ida.hefquin.cli;
 
-import java.io.File;
-
 import org.apache.jena.cmd.CmdGeneral;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-
 import se.liu.ida.hefquin.cli.modules.ModServer;
+
+import java.io.File;
 
 public class RunHeFQUINServer extends CmdGeneral
 {
@@ -90,7 +89,7 @@ public class RunHeFQUINServer extends CmdGeneral
 			String webappPath = RunHeFQUINServer.class.getClassLoader().getResource("webapp").toExternalForm();
 			webAppContext.setResourceBase(webappPath);
 		}
-
+		webAppContext.setMaxFormContentSize(10000000);
 		server.setHandler( webAppContext );
 
 		return server;
