4b2bbe9a7cc5f09a3fe2202b9b40c85b4296da15
theray1 <theray1@outlook.fr>
2025-03-06 17:14:30 +0100

    Changed RawCountAggregator back to its proper, simpler (and functional) version

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/RawCountAggregator.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/RawCountAggregator.java
@@ -1,21 +1,14 @@
 package se.liu.ida.hefquin.engine;
 
 import org.apache.jena.graph.Node;
-import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.binding.Binding;
-import org.apache.jena.sparql.expr.ExprList;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.expr.aggregate.Accumulator;
 import org.apache.jena.sparql.expr.aggregate.AccumulatorFactory;
 import org.apache.jena.sparql.expr.aggregate.AggCustom;
 import org.apache.jena.sparql.function.FunctionEnv;
-import se.liu.ida.hefquin.jenaintegration.sparql.FrawConstants;
 
-import java.util.List;
-import java.util.Objects;
-
-import static se.liu.ida.hefquin.jenaintegration.sparql.FrawConstants.VAR_GROUP_CURRENT_STAGE;
-import static se.liu.ida.hefquin.jenaintegration.sparql.FrawConstants.VAR_PROBABILITY_PREFIX;
+import static se.liu.ida.hefquin.jenaintegration.sparql.FrawConstants.MAPPING_PROBABILITY;
 
 public class RawCountAggregator {
 
@@ -27,64 +20,24 @@ public class RawCountAggregator {
 
         private Double numberOfWalks = 0.0;
         private Double estimation = 0.0;
-        private ExprList exprList;
 
-        public RawCountAccumulator(AggCustom agg, boolean distinct) {
-            exprList = agg.getExprList();
-        }
+        public RawCountAccumulator(AggCustom agg, boolean distinct) {}
 
         @Override
         public void accumulate(Binding binding, FunctionEnv functionEnv) {
-
-            List<Var> variablesToGroup = functionEnv.getContext().get(VAR_GROUP_CURRENT_STAGE);
-            Node totalProbaNode = binding.get(FrawConstants.MAPPING_PROBABILITY);
-
-            Double totalProbability;
-
-            try {
-                totalProbability = Double.valueOf(totalProbaNode.getLiteralValue().toString());
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("Could not parse total proba", e);
-            }
-
-            Double groupProbability = 1.0D;
-
-            if(Objects.nonNull(variablesToGroup)) {
-                for(Var var : variablesToGroup) {
-                    Node varProbaNode = binding.get(getProbaVarFromVar(var));
-
-                    Double varProbability;
-
-                    try {
-                        varProbability = Double.valueOf(varProbaNode.getLiteralValue().toString());
-                    } catch (NumberFormatException e) {
-                        throw new RuntimeException("Could not parse probability of " + varProbaNode.getName(), e);
-                    }
-
-                    groupProbability = groupProbability * varProbability;
-                }
+            Node n = binding.get(MAPPING_PROBABILITY);
+            if (n != null && n.isLiteral()) {
+                Double value = Double.valueOf(n.getLiteralValue().toString());
+                Double estimate = 1.0 / value;
+                estimation += estimate;
             }
 
             numberOfWalks += 1.0;
-            // 1.0 / (groupSpecificEstimation) = 1.0 / (totalProbability / groupProbability) = (groupProbability / totalProbability)
-            estimation += (groupProbability / totalProbability);
         }
 
         @Override
         public NodeValue getValue() {
-            return numberOfWalks == 0 ? NodeValue.makeInteger(0) : NodeValue.makeDouble(estimation/numberOfWalks);
-        }
-
-        private boolean isProbaVar(Var var){
-            return var.getVarName().contains(VAR_PROBABILITY_PREFIX);
-        }
-
-        private String getVarFromProbaVar(Var probaVar){
-            return probaVar.getVarName().split(VAR_PROBABILITY_PREFIX)[1];
-        }
-
-        private String getProbaVarFromVar(Var var){
-            return VAR_PROBABILITY_PREFIX + var.getName();
+            return NodeValue.makeDouble(estimation / numberOfWalks);
         }
     }
 }
