2021dd0d78247e3085a002f9cce1895409bb47b2
theray1 <theray1@outlook.fr>
2025-03-13 10:08:03 +0100

    Added FrawConstants file for constants used for the federated random walk process like the mapping probability var

---
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/jenaintegration/sparql/FrawConstants.java
@@ -0,0 +1,8 @@
+package se.liu.ida.hefquin.jenaintegration.sparql;
+
+import org.apache.jena.sparql.core.Var;
+
+public class FrawConstants {
+    public static final Var MAPPING_PROBABILITY           = Var.alloc("probabilityOfRetrievingRestOfMapping");
+}
+
