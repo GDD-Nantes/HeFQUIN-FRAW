ae2de30e4617d3137c68af4587c848bf9966901c
theray1 <theray1@outlook.fr>
2025-03-20 11:00:17 +0100

    Utility method

--- hefquin-base/src/main/java/se/liu/ida/hefquin/base/utils/Pair.java
+++ hefquin-base/src/main/java/se/liu/ida/hefquin/base/utils/Pair.java
@@ -5,6 +5,10 @@ public class Pair<T1,T2>
 	final public T1 object1;
 	final public T2 object2;
 
+	public static <T1, T2> Pair<T1, T2> of( final T1 object1, final T2 object2 ){
+		return new Pair(object1, object2);
+	}
+
 	public Pair ( final T1 object1, final T2 object2 ) {
 		this.object1 = object1;
 		this.object2 = object2;
