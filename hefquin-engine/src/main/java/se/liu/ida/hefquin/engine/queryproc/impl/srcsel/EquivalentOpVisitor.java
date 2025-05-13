ee0d20f4c035cb892e3096bd7c02a2fb9ac9519f
theray1 <theray1@outlook.fr>
2025-05-13 16:19:29 +0200

    Added extractors, changed TripleToSourceAndFilterExtractor by moving subclass, and changed EquivalentOpVisitor to better handle cross domain BGPs

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/srcsel/EquivalentOpVisitor.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/srcsel/EquivalentOpVisitor.java
@@ -4,17 +4,9 @@ import fr.gdd.fedqpl.visitors.ReturningArgsOpVisitor;
 import fr.gdd.fedqpl.visitors.ReturningArgsOpVisitorRouter;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.op.*;
+import se.liu.ida.hefquin.engine.OpVisitorRouter;
 
 public class EquivalentOpVisitor extends ReturningArgsOpVisitor<Boolean, Op> {
-
-    public Boolean visit(OpJoin op, Op other) {
-        if( ! (other instanceof OpJoin) ) {
-            return false;
-        }
-
-        return ReturningArgsOpVisitorRouter.visit(this, op.getLeft(), ((OpJoin) other).getLeft())
-            && ReturningArgsOpVisitorRouter.visit(this, op.getRight(), ((OpJoin) other).getRight());
-    }
 
     public Boolean visit(OpLeftJoin op, Op other) {
         if( ! (other instanceof OpLeftJoin) ) {
@@ -34,53 +26,44 @@ public class EquivalentOpVisitor extends ReturningArgsOpVisitor<Boolean, Op> {
                 && ReturningArgsOpVisitorRouter.visit(this, op.getRight(), ((OpUnion) other).getRight());
     }
 
-    public Boolean visit(OpBGP op, Op other) {
-        if( other instanceof OpTriple ) return ((OpTriple) other).equivalent(op);
-
-        if( ! (other instanceof OpBGP) ) {
-            return false;
-        }
-
-        return op.equalTo(other, null);
+    public Boolean visit(OpBGP op, Op other){
+        return areJoinEquivalent(op, other);
     }
 
     public Boolean visit(OpTriple op, Op other) {
-        if( other instanceof OpBGP ) return op.equivalent((OpBGP) other);
-
-        return op.equalTo(other, null);
+        return areJoinEquivalent(op, other);
     }
 
     public Boolean visit(OpService op, Op other) {
-        if( ! (other instanceof OpService) ) {
-            return false;
-        }
-
-        return ReturningArgsOpVisitorRouter.visit(this, op.getSubOp(), ((OpService) other).getSubOp());
+        return areJoinEquivalent(op, other);
     }
 
     public Boolean visit(OpSequence op, Op other) {
-        if( ! (other instanceof OpSequence) ) {
-            return false;
-        }
-
-        for( int i=0; i < op.size(); i++ ) {
-            Op subOp = op.get(i);
-            Op otherSubOp = ((OpSequence) other).get(i);
-            if( ! ReturningArgsOpVisitorRouter.visit(this, subOp, otherSubOp ) ) return false;
-        }
-
-        return true;
+        return areJoinEquivalent(op, other);
     }
 
     public Boolean visit(OpFilter op, Op other) {
-        if( ! (other instanceof OpFilter) ) {
+        return areJoinEquivalent(op, other);
+    }
+
+    public Boolean visit(OpJoin op, Op other) {
+        return areJoinEquivalent(op, other);
+    }
+
+    private static boolean areJoinEquivalent(Op op1, Op op2) {
+        try{
+            SourcedJoinTripleExtractor extractorOp1 = new SourcedJoinTripleExtractor();
+            OpVisitorRouter.visit(extractorOp1, op1);
+
+            SourcedJoinTripleExtractor extractorOp2 = new SourcedJoinTripleExtractor();
+            OpVisitorRouter.visit(extractorOp2, op2);
+
+            boolean triplesEqual = extractorOp1.getTriples().equals(extractorOp2.getTriples());
+            boolean filtersEqual = extractorOp1.getFilters().equals(extractorOp2.getFilters());
+
+            return triplesEqual && filtersEqual;
+        }catch (UnsupportedOperationException e){
             return false;
         }
-
-        OpFilter otherFilter = (OpFilter) other;
-
-        if ( ! op.getExprs().equals( otherFilter.getExprs() ) ) return false;
-
-        return ReturningArgsOpVisitorRouter.visit(this, op.getSubOp(), ((Op1) other).getSubOp());
     }
 }
--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/srcsel/ServiceContentExtractor.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/srcsel/ServiceContentExtractor.java
@@ -1,8 +1,5 @@
 package se.liu.ida.hefquin.engine.queryproc.impl.srcsel;
 
-import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
-import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.sparql.algebra.OpVisitorBase;
 import org.apache.jena.sparql.algebra.op.*;
@@ -12,8 +9,8 @@ import se.liu.ida.hefquin.engine.OpVisitorRouter;
 import java.util.HashSet;
 import java.util.Set;
 
-public class TripleToSourceAndFilterExtractor extends OpVisitorBase {
-    MultiValuedMap<Triple, Node> triples2Source = new HashSetValuedHashMap<>();
+public class ServiceContentExtractor extends OpVisitorBase {
+    Set<Triple> triples = new HashSet<>();
     Set<Expr> filters = new HashSet<>();
 
     public void visit(OpJoin op) {
@@ -27,23 +24,26 @@ public class TripleToSourceAndFilterExtractor extends OpVisitorBase {
     }
 
     public void visit(OpUnion op) {
-        OpVisitorRouter.visit(this, op.getLeft());
-        OpVisitorRouter.visit(this, op.getRight());
+        throw new IllegalArgumentException("Union not implemented yet");
     }
 
-    public void visit(OpService op) {
-        ServiceContentExtractor sce = new ServiceContentExtractor();
-        OpVisitorRouter.visit(sce, op.getSubOp());
-        sce.getTriples().forEach(
-                t -> triples2Source.put(t, op.getService())
-        );
-        sce.getFilters().forEach(
-                f -> filters.add(f)
-        );
+    public void visit(OpTriple op) {
+        this.triples.add(op.getTriple());
+    }
+
+    public void visit(OpBGP op) {
+        op.getPattern().getList().forEach(this.triples::add);
+    }
+
+    public void visit(OpSequence op) {
+        op.getElements().forEach(e -> OpVisitorRouter.visit(this, e));
     }
 
     public void visit(OpFilter op) {
         filters.addAll(op.getExprs().getList());
         OpVisitorRouter.visit(this, op.getSubOp());
     }
+
+    public Set<Triple> getTriples() { return triples; }
+    public Set<Expr> getFilters() { return filters; }
 }
